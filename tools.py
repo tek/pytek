a9cc48cbe2b7218a78dbfcd406748f542889c266
tek <tek@moonitor.org>
2008-11-16 17:08:06 +0100

    add a context manager for redirecting stdout

--- tools.py
+++ tools.py
@@ -1,3 +1,18 @@
+import sys
+
 def zip_fill(default, *seqs):
     filler = lambda *seq: [el if el is not None else default for el in seq]
     return map(filler, *seqs)
+
+class Silencer(object):
+    """ Context manager that suppresses output to stdout.
+
+    """
+    def __enter__(self):
+        sys.stdout = self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout = sys.__stdout__
+
+    def write(self, data):
+        pass
