76faa1859b6b15612fbe64b17d83fa8afbacdce2
tek <tek@moonitor.org>
2009-09-15 01:22:03 +0200

    printer function argument for str_list that is called for each element

--- tools.py
+++ tools.py
@@ -1,3 +1,20 @@
+""" {{{ Copyright (c) 2009 Torsten Schmits
+
+This file is part of pytek. pytek is free software;
+you can redistribute it and/or modify it under the terms of the GNU General
+Public License version 2, as published by the Free Software Foundation.
+
+pytek is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place, Suite 330, Boston, MA  02111-1307  USA
+
+}}} """
+
 import sys
 from itertools import izip
 
@@ -21,8 +38,9 @@ class Silencer(object):
 def repr_params(*params):
     return '(' + ', '.join(map(repr, params)) + ')'
 
-def str_list(l, j=', '):
-    return j.join(map(str, l))
+def str_list(l, j=', ', printer=lambda s: s):
+    strings = map(printer, l)
+    return j.join(map(unicode, strings))
 
 def choose(lst, indicator):
     return [l for l, i in izip(lst, indicator) if i]
