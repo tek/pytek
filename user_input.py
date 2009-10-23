8cf54b0ab9969782ffc4647c75e66a3c98bbeab2
tek <tek@moonitor.org>
2009-10-23 02:46:27 +0200

    enhance user_input

--- user_input.py
+++ user_input.py
@@ -29,13 +29,17 @@ class UserInput(object):
         """
         self._text = text
         self._validator = validator
-        self._validate = validate
+        self._do_validate = validate
         self._allow_args = args
         self.__init_attributes()
 
     def __init_attributes(self):
         self._input = None
         self._args = None
+        self._setup_validator()
+
+    def _setup_validator(self):
+        pass
 
     @property
     def value(self):
@@ -49,21 +53,39 @@ class UserInput(object):
         prompt = str_list(self._text, j='\n') + ' '
         if isinstance(prompt, unicode):
             prompt = prompt.encode('utf-8')
-        while self._read(prompt):
+        while not self._read(prompt):
             prompt = "Invalid input. Try again: "
         return self.value
 
     def _read(self, prompt):
-            self._input = raw_input(prompt)
-            return self._validate and self._validator and not \
-                   self._validator.match(str(self._input))
+        return self._do_input(raw_input(prompt))
+
+    def input(self, input):
+        if self._do_input(input):
+            return self.value
+        else:
+            raise InvalidInput(input)
+
+    def _do_input(self, input):
+        self._input = input
+        return self._validate()
+
+    def _validate(self):
+        return not (self._do_validate and self._validator and not
+                    self._validator.match(str(self._input)))
 
 class SimpleChoice(UserInput):
-    def __init__(self, elements, text=['Choose one'], *args, **kwargs):
-        strings = map(str, elements)
-        validator = regex(r'^(%s)$' % '|'.join(strings))
-        text[-1] += ' [' + '/'.join(strings) + ']'
-        UserInput.__init__(self, text, validator)
+    def __init__(self, elements, text=['Choose one'], additional=[], *args,
+                 **kwargs):
+        self._elements = map(str, elements)
+        self._additional = map(str, additional)
+        if self._elements:
+            text[-1] += ' [' + '/'.join(self._elements) + ']'
+        UserInput.__init__(self, text, *args, **kwargs)
+
+    def _setup_validator(self):
+        self._validator = regex(r'^(%s)$' % '|'.join(self._elements +
+                                               self._additional))
 
 class YesNo(SimpleChoice):
     def __init__(self, text=['Confirm'], *args, **kwargs):
@@ -76,30 +98,31 @@ class YesNo(SimpleChoice):
     def __nonzero__(self):
         return self.value
 
-class SpecifiedChoice(UserInput):
+class SpecifiedChoice(SimpleChoice):
     """ Automatically supply enumeration for the strings available for
     choice and query for a number. Can additionally take custom strings
     as valid input.
 
     """
-    def __init__(self, elements, text, additional=[], *args, **kwargs):
-        self.choices = dict(enumerate(elements))
-        self.additional = additional
-        for k, v in self.choices.iteritems():
-            text.append(' [%d] %s' % (k + 1, v))
+    def __init__(self, elements, text, simple=[], *args, **kwargs):
+        self._choices = elements
+        self._simple = simple
+        for i, v in enumerate(self._choices):
+            text.append(' [%d] %s' % (i + 1, v))
         text.append("Enter your choice:")
-        validator = regex(r'^(%s)$' % '|'.join(map(str,
-                                                   xrange(1, len(elements) + 1))
-                                               + additional))
-        UserInput.__init__(self, text, validator, *args, **kwargs)
+        #validator = regex(r'^(%s)$' % '|'.join(map(str,
+                                                   #xrange(1, len(elements) + 1))
+                                               #+ simple))
+        elements = range(1, len(elements) + 1)
+        SimpleChoice.__init__(self, simple, text, elements, *args, **kwargs)
 
     @property
     def value(self):
-        if self._input in self.additional:
+        if self._input in self._simple:
             return self._input
-        elif self._input.isdigit():
-            return self.choices[int(self._input) - 1]
-        elif not self._validate:
+        elif self._input.isdigit() and 0 < int(self._input) <= len(self._choices):
+            return self._choices[int(self._input) - 1]
+        elif not self._do_validate:
             return self._input
         else:
             raise InternalError('SpecifiedChoice: strange input: ' + self._input)
