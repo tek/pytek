287a25a96f185effc46b7cddb4d64002d1f8992c
tek <tek@moonitor.org>
2010-03-17 16:32:33 +0100

    echo input only if it's not a newline

--- terminal.py
+++ terminal.py
@@ -244,7 +244,7 @@ class Terminal(object):
         fd = sys.stdin.fileno()
         oldterm = termios.tcgetattr(fd)
         newattr = termios.tcgetattr(fd)
-        newattr[3] = newattr[3] & ~termios.ICANON
+        newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
         termios.tcsetattr(fd, termios.TCSANOW, newattr)
         oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
         fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
@@ -254,6 +254,8 @@ class Terminal(object):
             while not done:
                 try:
                     c = sys.stdin.read(1)
+                    if c != '\n':
+                        self.write(c)
                     done = True
                 except IOError:
                     sleep(0.01)
