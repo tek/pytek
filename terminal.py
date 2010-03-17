76bebfc76b6fd399ae12cec1497af43edff2972e
tek <tek@moonitor.org>
2010-03-17 13:52:14 +0100

    do not suppress echo on keypress

--- terminal.py
+++ terminal.py
@@ -244,7 +244,7 @@ class Terminal(object):
         fd = sys.stdin.fileno()
         oldterm = termios.tcgetattr(fd)
         newattr = termios.tcgetattr(fd)
-        newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+        newattr[3] = newattr[3] & ~termios.ICANON
         termios.tcsetattr(fd, termios.TCSANOW, newattr)
         oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
         fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
