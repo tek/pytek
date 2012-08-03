7af48d89cd5db48022351081edfbc2c0349dcabd
tek <tek@gentoo64.net>
2012-08-03 15:18:46 +0200

    obsolete forward imports

--- tek/__init__.py
+++ tek/__init__.py
@@ -21,6 +21,5 @@ from .util.debug import dodebug
 from .log import logger, debug
 from .process import process, process_output
 from .errors import MooException
-from .command_line import command_line, PrefixPrinter
 from .user_input import YesNo
 from .config import Configurations, ConfigClient
