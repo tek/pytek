7a042aee31eef03d296f145d2dd0ba8b02a9417e
tek <tek@gentoo64.net>
2012-09-08 01:59:58 +0200

    imports

--- tek/cpu.py
+++ tek/cpu.py
@@ -14,7 +14,8 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, see <http://www.gnu.org/licenses/>.
 """
 
-import resource, functools
+import resource
+import functools
 
 from tek import logger
 
