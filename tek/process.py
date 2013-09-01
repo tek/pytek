55c35669f6545e9b37c1bd9e49fa238afb52ac53
tek <tek@gentoo64.net>
2013-09-01 23:00:56 +0200

    formatting

--- tek/process.py
+++ tek/process.py
@@ -3,7 +3,6 @@ from subprocess import Popen, PIPE
 def process(args, wait=True, pipe=True):
     """ Run process 'args' and return the Popen instance after it's
         finished.
-
     """
     kwargs = {}
     if pipe:
@@ -15,5 +14,6 @@ def process(args, wait=True, pipe=True):
     
 def process_output(args):
     """ runs process 'args' and returns a list containing the
-        stdoutput lines without trailing newline characters """
+        stdoutput lines without trailing newline characters
+    """
     return [line.rstrip('\n') for line in process(args).stdout.readlines()]
