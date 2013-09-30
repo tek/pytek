e1ae815117c2994e2e66c66b204a68eabb27aaf4
tek <tek@gentoo64.net>
2013-09-30 15:56:35 +0200

    convert process output to str before returning

--- tek/process.py
+++ tek/process.py
@@ -1,5 +1,6 @@
 from subprocess import Popen, PIPE
 
+
 def process(args, wait=True, pipe=True):
     """ Run process 'args' and return the Popen instance after it's
         finished.
@@ -11,9 +12,11 @@ def process(args, wait=True, pipe=True):
     if wait:
         proc.wait()
     return proc
-    
+
+
 def process_output(args):
     """ runs process 'args' and returns a list containing the
         stdoutput lines without trailing newline characters
     """
-    return [line.rstrip('\n') for line in process(args).stdout.readlines()]
+    return [str(line).rstrip('\n') for line in
+            process(args).stdout.readlines()]
