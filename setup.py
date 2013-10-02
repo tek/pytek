acab1fe13ff04cf406bbe7d2697e20dd38b1634d
tek <tek@gentoo64.net>
2013-10-02 15:04:57 +0200

    1.1.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ import glob
 from setuptools import setup, find_packages
 
 setup(name='tek',
-      version='1.1.1',
+      version='1.1.2',
       author='Torsten Schmits',
       author_email='torstenschmits@gmail.com',
       license='GPLv3',
