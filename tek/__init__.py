fa3081841d2a58cb1bea76f55037ba271002d55d
tek <tek@gentoo64.net>
2013-12-23 19:29:42 +0100

    bring Spec to root scope

--- tek/__init__.py
+++ tek/__init__.py
@@ -21,3 +21,4 @@ from .process import process, process_output
 from .errors import MooException
 from .user_input import YesNo
 from .config import Configurations, ConfigClient
+from tek.test import Spec
