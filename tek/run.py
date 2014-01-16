19b3dd90f0d3eaa7b96f450050ddf285a94d554d
tek <tek@gentoo64.net>
2014-01-16 02:13:58 +0100

    add decorator for cli entry points

--- tek/run.py
+++ tek/run.py
@@ -1,26 +1,10 @@
-__copyright__ = """ Copyright (c) 2010-2013 Torsten Schmits
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by the
-Free Software Foundation; either version 3 of the License, or (at your
-option) any later version.
-
-This program is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, see <http://www.gnu.org/licenses/>.
-"""
-
 import signal
 import sys
 import threading
+import functools
 
 from tek import dodebug, logger
 from tek.errors import MooException
-from tek.tools import str_list
 
 
 class Singleton(type):
@@ -78,3 +62,12 @@ def moo_run(func, handle_sigint=True, *a, **kw):
         logger.error(e)
         if dodebug:
             raise
+
+
+def cli(func):
+    @functools.wraps(func)
+    def wrapper(*a, **kw):
+        return moo_run(func, *a, **kw)
+    return wrapper
+
+__all__ = ['SignalManager', 'cli']
