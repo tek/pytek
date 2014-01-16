e5e6a851882651fefc8cbde3426f5ec18a55bdf5
tek <tek@gentoo64.net>
2014-01-16 02:18:53 +0100

    remove copyright headers, bump version

--- tek/__init__.py
+++ tek/__init__.py
@@ -1,19 +1,3 @@
-__copyright__ = """ Copyright (c) 2009-2014 Torsten Schmits
-
-This file is part of tek-utils. tek-utils is free software;
-you can redistribute it and/or modify it under the terms of the GNU General
-Public License version 2, as published by the Free Software Foundation.
-
-tek-utils is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-details.
-
-You should have received a copy of the GNU General Public License along with
-this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-Place, Suite 330, Boston, MA  02111-1307  USA
-
-"""
 
 from .util.debug import dodebug
 from .log import logger, debug
--- tek/errors.py
+++ tek/errors.py
@@ -1,19 +1,3 @@
-__copyright__ = """ Copyright (c) 2009-2011 Torsten Schmits
-
-This file is part of pytek. pytek is free software;
-you can redistribute it and/or modify it under the terms of the GNU General
-Public License version 2, as published by the Free Software Foundation.
-
-pytek is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-details.
-
-You should have received a copy of the GNU General Public License along with
-this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-Place, Suite 330, Boston, MA  02111-1307  USA
-
-"""
 
 class MooException(Exception):
     def __init__(self, msg=''):
