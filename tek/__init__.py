e814d6dfb84a5083c7a8bed52782c751025d6c00
tek <tek@gentoo64.net>
2014-01-18 18:27:26 +0100

    forward-import lazy_configurable

--- tek/__init__.py
+++ tek/__init__.py
@@ -1,10 +1,9 @@
-
 from .util.debug import dodebug
 from .log import logger, debug
 from .process import process, process_output
 from .errors import MooException
 from .user_input import YesNo
-from .config import Configurations, ConfigClient
+from .config import Configurations, ConfigClient, lazy_configurable
 try:
     from tek.test import Spec
 except ImportError:
