55827f4858c6a673bcd7dc35c5918e86cfd57885
tek <torstenschmits@gmail.com>
2014-03-24 14:44:34 +0100

    1.1.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import glob
 from setuptools import setup, find_packages
 
 setup(name='tek',
-      version='1.1.5',
+      version='1.1.6',
       author='Torsten Schmits',
       author_email='torstenschmits@gmail.com',
       license='GPLv3',
