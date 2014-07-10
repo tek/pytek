25245ff916b5d3a9ad905a6dc55a2b1822f312e4
tek <torstenschmits@gmail.com>
2014-07-10 07:48:39 +0200

    add forward imports to __all__

--- tek/__init__.py
+++ tek/__init__.py
@@ -4,7 +4,13 @@ from .process import process, process_output
 from .errors import MooException
 from .user_input import YesNo
 from .config import Configurations, ConfigClient, lazy_configurable, Config
+from tek.run import cli
+
 try:
     from tek.test import Spec
 except ImportError:
     pass
+
+__all__ = ['cli', 'Spec', 'Configurations', 'ConfigClient',
+           'lazy_configurable', 'Config', 'YesNo', 'MooException', 'process',
+           'process_output', 'debug', 'logger', 'dodebug']
