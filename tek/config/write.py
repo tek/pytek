40e9a73af734ed77ab19cc8b158f4b8e49ff39b7
tek <torstenschmits@gmail.com>
2014-07-10 11:42:20 +0200

    clear metadata before loading configs for file output

--- tek/config/write.py
+++ tek/config/write.py
@@ -8,7 +8,6 @@ from tek.config import Config
 def config_names(_dir):
     modules = pkgutil.walk_packages([_dir], onerror=lambda x: True)
     for _l, name, ispkg in modules:
-        print(name)
         if not ispkg and '.' in name:
             pkg, module = name.rsplit('.', 1)
             if module == 'config':
@@ -19,6 +18,7 @@ def write_pkg_config(_dir, outfile, alias):
     sys.path[:0] = [_dir]
     Config.allow_files = False
     Config.allow_override = False
+    Config.clear_metadata()
     for name in config_names(_dir):
         try:
             Config.load_config(name)
