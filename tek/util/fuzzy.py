e09bdbee421b7ed25d95e60127ee19ba7ef8bef1
tek <torstenschmits@gmail.com>
2014-07-31 15:24:06 +0200

    compensate for empty list

--- tek/util/fuzzy.py
+++ tek/util/fuzzy.py
@@ -1,14 +1,14 @@
-
-from tek import logger
-
 from difflib import SequenceMatcher
 from operator import itemgetter
+
 
 def best_match(seq, target):
     matcher = SequenceMatcher()
     matcher.set_seq2(target)
+
     def ratio():
         for index, specimen in enumerate(seq):
-            matcher.set_seq1(specimen) 
+            matcher.set_seq1(specimen)
             yield index, matcher.ratio()
-    return max(ratio(), key=itemgetter(1))
+    candidates = ratio()
+    return max(candidates, key=itemgetter(1)) if candidates else None
