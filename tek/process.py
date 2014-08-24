a47ed0f6b8f70bbfe1b375607eebbf3d7b60da70
tek <torstenschmits@gmail.com>
2014-08-24 20:37:39 +0200

    replace process_output by built-in methods

--- tek/process.py
+++ tek/process.py
@@ -1,10 +1,10 @@
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, getoutput
 
 
 def process(args, wait=True, pipe=True):
-    """ Run process 'args' and return the Popen instance after it's
+    ''' Run process 'args' and return the Popen instance after it's
         finished.
-    """
+    '''
     kwargs = {}
     if pipe:
         kwargs.update(stdout=PIPE, stderr=PIPE)
@@ -15,8 +15,7 @@ def process(args, wait=True, pipe=True):
 
 
 def process_output(args):
-    """ runs process 'args' and returns a list containing the
-        stdoutput lines without trailing newline characters
-    """
-    return [line.decode().rstrip('\n') for line in
-            process(args).stdout.readlines()]
+    ''' runs process 'args' and returns a list containing the
+        stdout and stderr lines
+    '''
+    return getoutput(' '.join(args)).splitlines()
