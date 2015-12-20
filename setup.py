808e3691b6dc416426865f67265c019de3a5a0db
tek <torstenschmits@gmail.com>
2015-12-20 17:10:58 +0100

    1.1.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tek',
-      version='1.1.6',
+      version='1.1.7',
       author='Torsten Schmits',
       author_email='torstenschmits@gmail.com',
       license='GPLv3',
