45b704a2757baad82cc24c389ea4c501b0f41926
tek <torstenschmits@gmail.com>
2016-01-19 16:32:58 +0100

    2.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='tek',
-      version='2.0.0',
+      version='2.1.0',
       author='Torsten Schmits',
       author_email='torstenschmits@gmail.com',
       license='MIT',
